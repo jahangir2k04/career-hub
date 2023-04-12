b260778f9e458c60d54511c151620f30119a4ac7
jahangir2k04 <jahangir2k04gmail.com>
2023-04-12 18:20:46 +0600

    6 fob feature data displayed with all information

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import JobCategory from '../JobCategory.jsx/JobCategory';
+import { useLoaderData } from 'react-router-dom';
+import JobFeature from '../JobFeature/JobFeature';
 
 const Home = () => {
     const [categories, setCategories] = useState([]);
+
+    const jobFeatures = useLoaderData();
+    // console.log(jobFeatures);
     
     useEffect( () => {
 
@@ -17,12 +22,28 @@ const Home = () => {
             {/* job category section */}
             <section className='my-container'>
                 <h1 className='text-center text-5xl font-bold'>Job Category List</h1>
-                <p className='text-center mt-4 mb-8 text-gray-600'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                <p className='text-center mt-4 mb-8 text-gray-400'>Explore thousands of job opportunities with all the information you need. Its your future.</p>
 
                 <div className='grid grid-cols-1 md:grid-cols-4  gap-6 '>
                     {
                         categories.map(category => 
                         <JobCategory key={category.id} category={category}></JobCategory>)
+                    }
+                </div>
+            </section>
+
+            {/* job features section */}
+            <section className='my-container'>
+                <h1 className='text-5xl text-center font-bold '>Featured Jobs</h1>
+                <p className='text-center text-gray-400 mt-4 mb-8'>Explore  job features with all the information you need. Its your future.</p>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    {
+                        jobFeatures.map(jobFeature => 
+                        <JobFeature 
+                            key={jobFeature.id} 
+                            jobFeature={jobFeature}
+                        ></JobFeature>)
                     }
                 </div>
             </section>

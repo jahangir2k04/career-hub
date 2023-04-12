7c40eabdf8e209edeec5549b9601757418c56914
jahangir2k04 <jahangir2k04gmail.com>
2023-04-12 11:35:35 +0600

    nav bar added with mobile responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/solid'
+
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <div className=' md:mt-6 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+      <div className='relative flex items-center justify-between'>
+        {/* Logo Section */}
+        <Link to='/' className='inline-flex items-center'>
+          <BoltIcon className='h-6 w-6 text-violet-600' />
+          <span className='ml-2 text-4xl font-bold tracking-wide text-gray-800'>
+            Job Hunts
+          </span>
+        </Link>
+
+        {/* Nav Items Section */}
+        <ul className='items-center hidden space-x-8 lg:flex'>
+          <li>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/statistics'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/applied-jobs'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/blog'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+
+
+        {/* Mobile Navbar Section */}
+        <div className='lg:hidden'>
+          {/* Dropdown Open Button */}
+          <button
+            aria-label='Open Menu'
+            title='Open Menu'
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className='w-5 text-violet-600' />
+          </button>
+          
+          {isMenuOpen && (
+            <div className='absolute top-0 left-0 w-full z-10'>
+              <div className='p-5 bg-white border rounded shadow-sm'>
+                {/* Logo & Button section */}
+                <div className='flex items-center justify-between mb-4'>
+                  <div>
+                    <Link to='/' className='inline-flex items-center'>
+                      <BoltIcon className='h-6 w-6 text-violet-600' />
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                        Job Hunts
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label='Close Menu'
+                      title='Close Menu'
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className='w-5 text-violet-600' />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className='space-y-4'>
+                    <li>
+                      <Link to='/' className='default'>
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/statistics'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Statistics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/applied-jobs'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Applied Jobs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/blog'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/start-applying">
+                        <button className='my-bg-color text-white text-base font-bold px-5 py-3 rounded-lg'>
+                            Start Applying
+                        </button>
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <Link to="/start-applying" className='hidden md:block'>
+            <button className='my-bg-color text-white text-xl font-bold px-7 py-5 rounded-lg'>
+                Start Applying
+            </button>
+        </Link>
+      </div>
+    </div>
+  )
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Statistics from './components/Statistics/Statistics'
+import AppliedJobs from './components/AppliedJobs/AppliedJobs'
+import Blog from './components/Blog/Blog'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: 'applied-jobs',
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>,
+      }
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)

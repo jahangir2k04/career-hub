fd90253323fed62d1118a9dde873947264e5ad79
jahangir2k04 <jahangir2k04gmail.com>
2023-04-12 14:59:53 +0600

    job categories added with mobile responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import JobCategory from '../JobCategory.jsx/JobCategory';
 
 const Home = () => {
+    const [categories, setCategories] = useState([]);
+    
+    useEffect( () => {
+
+        fetch('jobCategory.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+
+    } , []); 
+
     return (
         <div>
-            
+            {/* job category section */}
+            <section className='my-container'>
+                <h1 className='text-center text-5xl font-bold'>Job Category List</h1>
+                <p className='text-center mt-4 mb-8 text-gray-600'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+                <div className='grid grid-cols-1 md:grid-cols-4  gap-6 '>
+                    {
+                        categories.map(category => 
+                        <JobCategory key={category.id} category={category}></JobCategory>)
+                    }
+                </div>
+            </section>
         </div>
     );
 };
---
+++ src/components/JobCategory.jsx/JobCategory.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const JobCategory = ({category}) => {
+    console.log(category);
+    const {img, name, job_abl} = category;
+
+    return (
+        <div className='p-10 header-bg rounded-lg'>
+            <img className='p-4 bg-violet-100 rounded-lg' src={img} alt="" />
+            <h3 className='mt-8 mb-2 text-xl font-bold'>{name}</h3>
+            <p className='text-gray-500'>{job_abl}+ Jobs Available</p>
+        </div>
+    );
+};
+
+export default JobCategory;

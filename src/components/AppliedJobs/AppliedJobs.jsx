a59c683de0ee7d7d314f37fbd393a340cd6c1876
jahangir2k04 <jahangir2k04gmail.com>
2023-04-14 03:22:53 +0600

    filtering added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -20,15 +20,31 @@ const AppliedJobs = () => {
 
     }, []);
 
+
+    const handleFilter = (jobType) => {
+        const storedAppliedJobs = getAppliedJobs();
+        // const saveAppliedJobs = [];
+
+        const addedAppliedJobs = allJobs.filter(job => job.environment.type === jobType);
+        // saveAppliedJobs.push(addedAppliedJobs);
+        // setAppliedJobs(saveAppliedJobs);
+        setAppliedJobs(addedAppliedJobs);
+    }
+
+
     return (
         <div className='my-container'>
             <h3 className='text-center text-5xl font-bold mb-8'>Applied Jobs</h3>
-            
+            <div className='flex justify-end gap-6 mb-6'>
+                <button onClick={() => handleFilter('Onsite')} className='w-36 my-bg-color py-3 px-4 text-xl font-bold text-white rounded'>Onsite</button>
+
+                <button onClick={() => handleFilter('Remote')} className='w-36 my-bg-color py-3 px-4 text-xl font-bold text-white rounded'>Remote</button>
+            </div>
 
             <div>
                 {
                     appliedJobs.map(job => <AppliedJob
-                        key={job}
+                        key={job.id}
                         job={job}
                     ></AppliedJob>)
                 }
